5ca8b74b150f20f45dc7405ed58658fa56e33f29
XieDeSheng <wb.xds@mesg.corp.netease.com>
2023-04-26 19:01:54 +0800

    fix: 提交活动的

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   transpilePackages: ['antd-mobile'],
 
   reactStrictMode: true,
+  async rewrites () {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://www.nfcmdx.top/api/:path*',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
