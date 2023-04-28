ba246f5eae90e3c4ecf75fb94fb1f70019451c0d
XieDeSheng <wb.xds@mesg.corp.netease.com>
2023-04-28 11:05:29 +0800

    fix: 增加网站备案信息

---
+++ components/layouts.tsx
@@ -0,0 +1,9 @@
+
+export default function Layout({ children }) {
+    return <>
+      {children}
+      <div className='icp'>
+        <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2022046023号-1</a>
+      </div>
+    </>
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
 import type { AppProps } from 'next/app'
 import '../styles/global.css'
+import '../styles/normalize.css'
+import Layout from '@/components/layouts'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Layout>
+    <Component {...pageProps} />
+  </Layout>
 }
